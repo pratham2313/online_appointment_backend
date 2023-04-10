4266252a4bd6cd17115d658ab335d36bb3796778
Pratham <p.pratham2313@gmail.com>
2023-04-10 13:38:20 +0530

    remove videocall

--- server.js
+++ server.js
@@ -853,49 +853,49 @@ app.post("/sendvideocallid", async (req, res) => {
 
 
 //............................. Audio Video call .......................//
-const http = require("http");
-const { send } = require('process');
-const { error } = require('console');
-const socket = http.createServer(app)
-const io = require("socket.io")(socket, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-    }
-})
+// const http = require("http");
+// const { send } = require('process');
+// const { error } = require('console');
+// const socket = http.createServer(app)
+// const io = require("socket.io")(socket, {
+//     cors: {
+//         origin: "http://localhost:3000",
+//         methods: ["GET", "POST"]
+//     }
+// })
 
 
 
-//............ videocall functionality.............//
-io.on("connection", (socket) => {
-    // console.log(socket.id, 'connected');
-    const id = socket.id;
-    socket.emit("me", id);
-    // console.log(id);
-    socket.on("patientdisconnect", (data) => {
-        // socket.emit("endfromboth", { callend: data.callend });
-        io.to(data.userTocall).emit("endfrompatient", { callend: data.callend });
-    })
-    socket.on("doctordisconnect", (data) => {
-        console.log("disconnect from doctor");
-        // socket.emit("endfromboth", { callend: data.callend });
-        io.to(data.to).emit("endfromdoctor", { callend: data.callend });
-    })
-    socket.on("disconnect", (data) => {
+// //............ videocall functionality.............//
+// io.on("connection", (socket) => {
+//     // console.log(socket.id, 'connected');
+//     const id = socket.id;
+//     socket.emit("me", id);
+//     // console.log(id);
+//     socket.on("patientdisconnect", (data) => {
+//         // socket.emit("endfromboth", { callend: data.callend });
+//         io.to(data.userTocall).emit("endfrompatient", { callend: data.callend });
+//     })
+//     socket.on("doctordisconnect", (data) => {
+//         console.log("disconnect from doctor");
+//         // socket.emit("endfromboth", { callend: data.callend });
+//         io.to(data.to).emit("endfromdoctor", { callend: data.callend });
+//     })
+//     socket.on("disconnect", (data) => {
 
-        socket.broadcast.emit("callEnded")
-    })
+//         socket.broadcast.emit("callEnded")
+//     })
 
-    socket.on("callUser", (data) => {
-        console.log("server called data from patient")
-        io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
-    })
+//     socket.on("callUser", (data) => {
+//         console.log("server called data from patient")
+//         io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+//     })
 
-    socket.on("answerCall", (data) => {
-        io.to(data.to).emit("callAccepted", data.signal)
-    })
-})
-socket.listen(8001, () => console.log("Videocall server is running on port 8001"))
+//     socket.on("answerCall", (data) => {
+//         io.to(data.to).emit("callAccepted", data.signal)
+//     })
+// })
+// socket.listen(8001, () => console.log("Videocall server is running on port 8001"))
 
 
 
